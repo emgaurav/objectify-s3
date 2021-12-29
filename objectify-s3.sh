25345cf1f5876c3c4199cdd6b606fba532c3935d
Gaurav Arora <38912972+emgaurav@users.noreply.github.com>
2021-12-29 14:14:27 +0530

    Update objectify-s3.sh

--- objectify-s3.sh
+++ objectify-s3.sh
@@ -17,7 +17,7 @@ cd ~/objectify-s3; git reset --hard >/dev/null 2>&1; git pull;
 echo "$(tput setaf 2)Done $(tput sgr 0)"$'\n'
 echo "checking awscli configuration"
 if ! aws configure list-profiles|grep -q -i "default"; then
-	echo "awscli is not configured. You must configure using \'aws configure\'."
+	echo "$(tput bold)$(tput setaf 1)awscli is not configured. You must configure using 'aws configure' command.$(tput sgr 0)"$'\n'
 	exit 0
 fi
 	
