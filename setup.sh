29928cb2739f8ff9401a66cc991d69d02c5fc386
Gaurav Arora <38912972+emgaurav@users.noreply.github.com>
2021-12-31 17:36:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ else
 		echo "-----------------------------------"
 	fi
 fi
-echo "$(tput bold)Setting Alias$(tput sgr 0)"
+#echo "$(tput bold)Setting Alias$(tput sgr 0)"
 source ~/.bash_profile 2>/dev/null
 source ~/.bashrc 2>/dev/null
 source ~/.zshrc 2>/dev/null
